364c8a44049eefa78bfa2872926cbff0b2e239bc
JonTurner <JonTurner@users.noreply.github.com>
2023-08-08 00:37:41 -0400

    add comments to set.lua

--- lua/set.lua
+++ lua/set.lua
@@ -1,18 +1,17 @@
 vim.opt.guicursor = ""
 
 vim.opt.nu = true
-vim.opt.relativenumber = true
-
-vim.opt.tabstop = 4
+vim.opt.relativenumber = true -- Display the row number relative to the up/down rows
+vim.opt.tabstop = 4 -- Number of Spaces, a single tab counts for
 vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
-vim.opt.expandtab = true
+vim.opt.shiftwidth = 4 -- The default size of an indentation (this will be configure per filetype later on)
+vim.opt.expandtab = true -- Use spaces instead of tabs
 
 vim.opt.smartindent = true
 
-vim.opt.wrap = false
+vim.opt.wrap = false -- Configure Neovim to not wrap the contents of the buffer
 
-vim.opt.swapfile = false
+vim.opt.swapfile = false --Disable swapfile creation since it gets annoying after a while
 vim.opt.backup = false
 vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
 vim.opt.undofile = true
@@ -20,20 +19,18 @@ vim.opt.undofile = true
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
 
-vim.opt.termguicolors = true
+vim.opt.termguicolors = true -- True color support for various plugins
 
 -- keeps this many lines above or below the cursor
-vim.opt.scrolloff = 8
-vim.opt.signcolumn = "yes"
+vim.opt.scrolloff = 8 -- Maintain a buffer of rows between the current row & the either ends of the window
+vim.opt.signcolumn = "yes" -- Allow the sign column to show else it'll keep shifting later on
 vim.opt.isfname:append("@-@")
 
-vim.opt.updatetime = 50
+vim.opt.updatetime = 50 -- Save swap file & trigger CursorHold
 
 -- vim.opt.colorcolumn = "80"
--- set splitbelow
--- set splitright
-vim.opt.splitbelow = true
-vim.opt.splitright = true
+vim.opt.splitbelow = true -- Put new windows below current
+vim.opt.splitright = true -- Put new windows right of the current
 
 -- vimwiki/vimwiki settings
 vim.cmd('filetype plugin on')
@@ -42,3 +39,30 @@ vim.cmd('syntax on')
 -- no folding
 vim.wo.foldenable = false
 
+-- local opt = vim.opt
+
+-- opt.number = true -- Display the row number on the left side of the window
+-- opt.autowrite = true -- Save the contents of the buffer after exiting Insert mode
+-- opt.clipboard = "unnamedplus" -- Sync the system clipboard with Neovim
+-- opt.laststatus = 3 -- Enable the global status line
+-- opt.completeopt = "menu,menuone,noselect" -- Make the autocompletion window more accessible
+-- opt.conceallevel = 3 -- Hide the * markups for bold & italics
+-- opt.confirm = true -- Prompt to save the contents of the buffer before exiting
+-- -- TODO: Figure out what purpose does it serve
+-- -- opt.formatoptions = "jcroqlnt" -- Some formatting configuration, needs more research
+-- opt.ignorecase = true -- Ignore casing when searching for a word
+-- opt.inccommand = "nosplit" -- Preview incremental substitute
+-- opt.list = true -- Show invisible whitespace characters
+-- opt.pumblend = 10 -- Make the completion popup window a bit transparent
+-- opt.pumheight = 10 -- Display only 10 rows for the completion menu
+-- opt.shiftround = true -- Roundup indentations
+-- -- TODO: Figure a way out to configure these options specific to the filetype instead
+-- opt.showmode = false -- Disable showing the current mode since its already visible on the statusline
+-- opt.smartcase = true -- Don't ignore casing with capitals
+-- opt.spelllang = { "en" } -- The language to use when spelling checking
+-- opt.timeoutlen = 300 -- ??
+-- opt.undolevels = 10000 -- ??
+-- opt.wildmode = "longest:full,full" -- Command-line completion mode
+-- opt.winminwidth = 5 -- Maximum window width
+-- opt.iskeyword:append("-") -- Make Neovim recognise dash-seperated words as a single word
+-- opt.cursorline = true -- Configure Neovim to highlight the current location of the cursor
